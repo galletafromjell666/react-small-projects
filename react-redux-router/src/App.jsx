f2443c9c4d6f822c0b5de499574f309f51f098b9
galletafromjell666 <giococu666@gmail.com>
2023-01-05 22:01:15 -0600

    Add new project

---
+++ react-redux-router/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-orange-50 w-full ">
+        <h1 className="text-xl font-bold underline">Test</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
