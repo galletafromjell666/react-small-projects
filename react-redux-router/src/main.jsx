5a60392e316dde779f63928e901d2684164b0d82
galletafromjell666 <giococu666@gmail.com>
2023-01-07 13:40:23 -0600

    Add root loader

--- react-redux-router/src/main.jsx
+++ react-redux-router/src/main.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root, { rootLoader } from "./routes/root";
 import { ErrorPage } from "./routes/error-page";
 import Pokemon from "./routes/pokemon";
+import Index from "./routes";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "pokemon/:pokemonName",
         element: <Pokemon />,
--- react-redux-router/src/routes/root.jsx
+++ react-redux-router/src/routes/root.jsx
@@ -1,10 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
+
+export const rootLoader = async () => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=30`);
+ // console.log(response);
+  const pokm = await response.json();
+ // console.log(pokm);
+  return { pokm };
+};
 
 const Root = () => {
+  const { pokm: pokmData } = useLoaderData();
   return (
     <div className="w-full h-screen bg-slate-500">
       <div className="bg-stone-400 w-full h-10">root header</div>
       <div>
+        {pokmData.results.length ? (
+          <ul>
+            {pokmData.results.map((poke) => {
+              return <h1 key={poke.name}> {poke.name}</h1>;
+            })}
+          </ul>
+        ) : (
+          <h1>Null</h1>
+        )}
         <Outlet />
       </div>
     </div>
