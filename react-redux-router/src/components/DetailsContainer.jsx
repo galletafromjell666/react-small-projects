4b4692ad1247db4544dbf7ac25ce4c5b45994cf4
galletafromjell666 <giococu666@gmail.com>
2023-01-12 21:31:10 -0600

    Style detail containe

--- react-redux-router/src/components/DetailsContainer.jsx
+++ react-redux-router/src/components/DetailsContainer.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import PokeBall from "./../assets/pokeball.png";
 import EvolutionChain from "./EvolutionChain";
+import { Colors } from "../helpers/backgroundColor";
 const DetailsContainer = ({
   name,
   id,
@@ -9,12 +10,17 @@ const DetailsContainer = ({
   sprites,
   color: typeColor,
 }) => {
+  console.log(types);
   return (
     <div>
       <div
-        className="curve flex flex-col content-center items-center"
-        style={{'--bgColor': typeColor}}
+        className="curve flex flex-col py-6"
+        style={{ "--bgColor": typeColor }}
       >
+        <div className="w-full flex flex-row px-6 pb-4 justify-between">
+          <h1 className="text-2xl capitalize font-bold">{name}</h1>
+          <span className="text-lg">#{id.toString().padStart(3, 0)}</span>
+        </div>
         <div className="relative w-full h-[400px]">
           <img
             className="absolute z-50 m-auto left-0 right-0 h-full"
@@ -25,14 +31,20 @@ const DetailsContainer = ({
             src={PokeBall}
           />
         </div>
-
-        
       </div>
-      <div>
-        <span className="text-lg">{id}</span>
-        <h1 className="text-2xl font-bold">{name}</h1>
+      <div className="flex flex-row justify-center space-x-8 my-8 capitalize">
         {types.map((type, index) => {
-          return <h2 key={index}>{type.name}</h2>;
+          return (
+            <div
+              key={index}
+              className="flex items-center content-center font-normal w-[175px]  capitalize text-xl rounded-full md:text-xl"
+              style={{ backgroundColor: Colors[type.type.name] }}
+            >
+              <h2 className="w-full px-6 py-[6px] text-center">
+                {type.type.name}
+              </h2>
+            </div>
+          );
         })}
       </div>
       <h3 className="text-md">{JSON.stringify(stats)}</h3>
