0a9c0880b4c51da9205dd6c57dce3c92b8c2af78
galletafromjell666 <giococu666@gmail.com>
2023-01-15 10:03:08 -0600

    Remove project

--- react-redux-router/src/components/PokemonTypeIcon.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-const PokemonTypeIcon = ({ type }) => {
-  return (
-    <img className="drop-shadow-lg" style={{width:'80px'}} src={`/types_icons/Pokemon_Type_Icon_${type}.svg`} />
-  );
-};
-
-export default PokemonTypeIcon;
